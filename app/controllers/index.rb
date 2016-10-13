28dbe0c921298319b0ff08647ba76b16148a8e79
Ellie Dori & Robbie Santos <pair+elliedori.robbiealixsantos@devbootcamp.com>
2016-10-13 15:45:14 -0700

    Add erb page for showing a single question and its associated content

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,9 @@ get '/' do
   @container = Post.all.order(created_at: :desc)
 	erb :index
 end
+
+get '/questions/:id' do
+  id = params[:id]
+  @question = Question.find(id)
+  erb :"questions/show"
+end
