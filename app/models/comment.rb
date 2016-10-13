25d845f3f2e62da20d0db08556f8e16a323c34f8
Ellie Dori & Robbie Santos <pair+elliedori.robbiealixsantos@devbootcamp.com>
2016-10-13 14:26:04 -0700

    Add foreign key to associations where needed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :commenter, class_name: 'User'
+  belongs_to :commenter, class_name: 'User', foreign_key: 'user_id'
   belongs_to :commentable, polymorphic: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  belongs_to :questioner, class_name: 'User'
+  belongs_to :questioner, class_name: 'User', foreign_key: 'user_id'
   has_many :comments, as: :commentable
   has_one :post
   has_many :answers
